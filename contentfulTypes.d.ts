fb81d7353731ba7054d8292b27eb03f190769860
Matt Albrecht <matthewwilliamalbrecht@gmail.com>
2023-03-03 09:12:33 -0500

    initial work

---
+++ contentfulTypes.d.ts
@@ -0,0 +1,15 @@
+import { Entry, Asset } from 'contentful'
+export const CheckIn = 'checkIn'
+import type { Document } from '@contentful/rich-text-types';
+
+export interface CheckIn {
+  //Check In
+  /*  */
+  readonly date: string
+  readonly location?: string
+  readonly media?: ReadonlyArray<Asset>
+  readonly mileMarker: number
+  readonly note?: Document
+  readonly title: string
+}
+
