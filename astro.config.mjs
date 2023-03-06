58229b3d053193231f90d0b393b64ce470ddf3f4
Matt Albrecht <matthewwilliamalbrecht@gmail.com>
2023-03-06 10:08:35 -0500

    remove image

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
-import image from '@astrojs/image'
-
-// https://astro.build/config
 import react from '@astrojs/react'
 
 // https://astro.build/config
@@ -15,6 +12,6 @@ import netlify from '@astrojs/netlify/functions'
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind(), image(), react()],
+  integrations: [tailwind(), react()],
   adapter: netlify(),
 })
